22614e0bc4f1d09aa9c03e637e3d1b6492d18aeb
postgrescms <postgrescms@gmail.com>
2022-07-13 01:09:32 +0300

    start test data generator

--- demo/react/index.js
+++ demo/react/index.js
@@ -1,20 +1,22 @@
 const dbDescription = {
   entities: {
-    table: {
+    project: {
       attributes: {
-        table_key: {
+        project_key: {
           isPrimaryKey: true
         },
-        f1: {
-          title: "first"
+        name: {
+          title: "Name"
         },
-        f2: {
-          hideInForm: true,
-          title: "second"
+        cost: {
+          title: "Cost, $"
         },
-        f3: {
+        deadline: {
+          title: "Deadline"
+        },
+        info: {
           hideInTable: true,
-          title: "third"
+          title: "Brief information"
         }
       }
     },
@@ -66,12 +68,12 @@ const dbDescription = {
   },
   relationships: [{
     title: "c1 rel",
-    parent: "table",
+    parent: "project",
     child: "child1",
     childKey: "e_key"
   },
   {
-    parent: "table",
+    parent: "project",
     child: "child2",
     childKey: "e_key"
   },
@@ -82,15 +84,42 @@ const dbDescription = {
   }]
 };
 
+const stringComponents = {
+  project: {
+    name: [["Tannotis", "Edogawa", "Yicilles", "Onkeon", "Thater", "Chaunus", "Zeitera", "Suumia", "Thoth SSUL"],
+    ["Library", "Software", "Framework", "Components", "Builder", "Backend"],
+    ["1.0", "2.0", "3.0", "4.0", "Alpha", "Beta", "Release"]]
+  }
+}
+
+function getRandomDate(init, days) {
+  var someDate = new Date(init);
+  someDate.setDate(someDate.getDate() + days);
+  return someDate.toISOString().substring(0, 10);
+}
+
+function getStringFromComponents(components, hash) {
+  return components.map((comp, i) => comp[hash * (i * 1997 + 1) * 182368273 % comp.length]).join(" ");
+}
+
 var db = {};
-db["table"] = [];
+db["project"] = [];
 db["child1"] = [];
 db["child2"] = [];
 db["grandchild"] = [];
 
 for (let i = 0; i < 23; i++) {
-  db["table"].push({ key: "table_" + i, data: { table_key: "table_" + i, f1: "table_f1_" + i, f2: "table_f2_" + i, f3: "table_f3_" + i } })
+  db["project"].push({
+    key: i,
+    data: {
+      project_key: i,
+      name: getStringFromComponents(stringComponents.project.name, 78423629387 * i % 1926283),
+      info: "table_f2_" + i, cost: (i * 11217 + 113943).toString().substring(0, (i * 163 % 3 + 4)),
+      deadline: getRandomDate('2022-01-01', (i * 1117 + 113) % 993)
+    }
+  })
 }
+/*
 for (let i = 0; i < 200; i++) {
   db["child1"].push({ key: "child1_" + i, data: { child_key: "child1_" + i, e_key: "table_" + (i * 17 % 23), f1: "child1_f1_" + i, f2: "child1_f2_" + i } })
 }
@@ -100,6 +129,7 @@ for (let i = 0; i < 100; i++) {
 for (let i = 0; i < 3000; i++) {
   db["grandchild"].push({ key: "grandchild_" + i, data: { g_child_key: "grandchild_" + i, c_key: "child1_" + (i * 1371 % 200), f1: "grandchild_f1_" + i, f2: "grandchild_f2_" + i } })
 }
+*/
 console.log(db);
 
 function getData(entityId, attributes, options, ready) {
@@ -131,4 +161,4 @@ let element = document.createElement("div");
 element.id = "explorerElement";
 document.body.appendChild(element);
 ReactDOM.render(<JSDataExplorer.Explorer model={explorerViewModel} />, element);
-explorerViewModel.start("table");
+explorerViewModel.start("project");
