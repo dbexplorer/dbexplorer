aefa9f6f2e5e72deba9328a8b9256a1e4c44bf8c
postgrescms <postgrescms@gmail.com>
2022-07-14 00:52:33 +0300

    test data for second table

--- demo/react/index.js
+++ demo/react/index.js
@@ -20,18 +20,24 @@ const dbDescription = {
         }
       }
     },
-    child1: {
-      title: "child table 1",
+    task: {
+      title: "Task",
       attributes: {
-        child_key: {
+        task_key: {
           isPrimaryKey: true
         },
-        e_key: {},
-        f1: {
-          title: "c1 first"
+        project_key: {},
+        title: {
+          title: "Title"
         },
-        f2: {
-          title: "c1 second"
+        due_time: {
+          title: "Due time"
+        },
+        progress: {
+          title: "Progress"
+        },
+        description: {
+          title: "Description"
         },
       }
     },
@@ -67,10 +73,10 @@ const dbDescription = {
     }
   },
   relationships: [{
-    title: "c1 rel",
+    title: "Tasks",
     parent: "project",
-    child: "child1",
-    childKey: "e_key"
+    child: "task",
+    childKey: "project_key"
   },
   {
     parent: "project",
@@ -89,6 +95,9 @@ const stringComponents = {
     name: [["Tannotis", "Edogawa", "Yicilles", "Onkeon", "Thater", "Chaunus", "Zeitera", "Suumia", "Thoth SSUL"],
     ["Library", "Software", "Framework", "Components", "Builder", "Backend"],
     ["1.0", "2.0", "3.0", "4.0", "Alpha", "Beta", "Release"]]
+  },
+  task: {
+    title: [["Implement", "Write", "Create"], ["code", "tests", "prototype", "debug data"]]
   }
 }
 
@@ -98,31 +107,47 @@ function getRandomDate(init, days) {
   return someDate.toISOString().substring(0, 10);
 }
 
+function getRandomText(hash) {
+  let characters = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+  return characters.substring(0, 200 + (hash * 196129387) % 220);
+}
+
 function getStringFromComponents(components, hash) {
-  return components.map((comp, i) => comp[hash * (i * 1997 + 1) * 182368273 % comp.length]).join(" ");
+  return components.map((comp, i) => comp[hash * ((i + 1) * 1997 + 1242) * 182368273 % 1238421 % comp.length]).join(" ");
 }
 
 var db = {};
 db["project"] = [];
-db["child1"] = [];
+db["task"] = [];
 db["child2"] = [];
 db["grandchild"] = [];
 
-for (let i = 0; i < 23; i++) {
+for (let i = 0; i < 57; i++) {
   db["project"].push({
     key: i,
     data: {
       project_key: i,
-      name: getStringFromComponents(stringComponents.project.name, 78423629387 * i % 1926283),
-      info: "table_f2_" + i, cost: (i * 11217 + 113943).toString().substring(0, (i * 163 % 3 + 4)),
-      deadline: getRandomDate('2022-01-01', (i * 1117 + 113) % 993)
+      name: getStringFromComponents(stringComponents.project.name, 78423629387 * (i + 1) % 1926283),
+      info: getRandomText(172361278 * (i + 1) % 1823023),
+      cost: (i * 11217 + 113943).toString().substring(0, ((i + 1) * 163 % 3 + 4)),
+      deadline: getRandomDate('2022-01-01', ((i + 1) * 1117 + 113) % 993)
+    }
+  })
+}
+
+for (let i = 0; i < 200; i++) {
+  db["task"].push({
+    key: i, data: {
+      task_key: i,
+      project_key: (i * 17 % 57),
+      title: getStringFromComponents(stringComponents.task.title, 34534515 * (i + 13452345) % 11349267),
+      description: getRandomText(172361278 * (i + 1) % 182303),
+      progress: (i + 123) * 18236 % 75 + 25,
+      due_time: getRandomDate('2022-01-01', ((i + 1) * 1117 + 113) % 756)
     }
   })
 }
 /*
-for (let i = 0; i < 200; i++) {
-  db["child1"].push({ key: "child1_" + i, data: { child_key: "child1_" + i, e_key: "table_" + (i * 17 % 23), f1: "child1_f1_" + i, f2: "child1_f2_" + i } })
-}
 for (let i = 0; i < 100; i++) {
   db["child2"].push({ key: "child2_" + i, data: { child_key: "child2_" + i, e_key: "table_" + (i * 19 % 23), f1: "child1_f1_" + i, f2: "child1_f2_" + i } })
 }
