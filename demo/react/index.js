2291f8142bd5c519049fd36d7b06e35b008b7d90
postgrescms <postgrescms@gmail.com>
2022-07-31 22:43:31 +0300

    upgrade index rendering to React18

--- demo/react/index.js
+++ demo/react/index.js
@@ -130,5 +130,7 @@ explorerViewModel.getDataCallback = getData;
 let element = document.createElement("div");
 element.id = "explorerElement";
 document.body.appendChild(element);
-ReactDOM.render(<JSDataExplorer.Explorer model={explorerViewModel} />, element);
+const root = ReactDOM.createRoot(element);
+root.render(<JSDataExplorer.Explorer model={explorerViewModel} />);
+
 explorerViewModel.start("table");
